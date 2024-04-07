3a717dfdb1889cd1a3410aee4af769f039877a38
Nik <yakubamykyta@gmail.com>
2024-04-07 15:42:55 +0300

    make connecting to the db a function

--- db_connect.php
+++ db_connect.php
@@ -1,13 +1,14 @@
 <?php
-$servername = "localhost";
-$username = "book_admin";
-$password = "book_admin";
-
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=bookstore", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-} catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+function connect($servername = "localhost",
+                 $dbname = "bookstore",
+                 $username = "book_admin",
+                 $password = "book_admin")
+{
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+        die();
+    }
+    return $conn;
 }
