58014107e9c53ab053e0e3a3d1b11c46305e597f
Nik <yakubamykyta@gmail.com>
2024-04-10 15:04:48 +0300

    add isUserExists function

--- queries.php
+++ queries.php
@@ -5,10 +5,12 @@ function getBooks(PDO $pdo): false|PDOStatement
     return $pdo->query($query);
 }
 
-function getBookByID(PDO $pdo, int $id)
+function getBookById(PDO $pdo, int $id)
 {
-    $query = "SELECT * FROM book WHERE id = $id";
-    $book = $pdo->query($query)->fetch(PDO::FETCH_ASSOC);
+    $query = "SELECT * FROM book WHERE id = :id";
+    $statement = $pdo->prepare($query);
+    $statement->execute(['id' => $id]);
+    $book = $statement->fetch(PDO::FETCH_ASSOC);
     if (!$book) {
         $book = [
             'title' => "Book doesn't exist",
@@ -17,4 +19,12 @@ function getBookByID(PDO $pdo, int $id)
         ];
     }
     return $book;
+}
+
+function isUserExists(PDO $pdo, string $username): bool
+{
+    $query = "SELECT name FROM customers WHERE name = :name";
+    $statement = $pdo->prepare($query);
+    $statement->execute(['name' => $username]);
+    return boolval($statement->fetch(PDO::FETCH_ASSOC));
 }
