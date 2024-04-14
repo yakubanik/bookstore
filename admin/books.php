79e9748e78cad1a5573fc13084bf96d3325a10e0
Nik <yakubamykyta@gmail.com>
2024-04-14 18:29:49 +0300

    add books to the admin panel

--- admin/books.php
+++ admin/books.php
@@ -17,7 +17,34 @@ $books = getBooks(connect());
 <a href="books.php">Books</a>
 <a href="">Users</a>
 <a href="">Orders</a>
-
+<table>
+    <thead>
+    <tr>
+        <th>Title</th>
+        <th>Author</th>
+        <th>Price</th>
+    </tr>
+    </thead>
+    <?php foreach ($books as $book): ?>
+        <tr>
+            <td><a href="../book_page.php?id=<?php echo $book['id']; ?>">
+                    <?php echo $book['title'] ?></a></td>
+            <td><?php echo $book['author'] ?></td>
+            <td><?php echo $book['price'] ?></td>
+            <td><a href="edit_book.php?id=<?= $book['id'] ?>">
+                    <button>edit</button>
+                </a></td>
+            <td><a>
+                    <button>delete</button>
+                </a></td>
+        </tr>
+    <?php endforeach; ?>
+    <tr>
+        <td><a>
+                <button>add new</button>
+            </a></td>
+    </tr>
+</table>
 
 </body>
 </html>
